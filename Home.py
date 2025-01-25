6a627d3a16007ec26e553c3d85f0f765bf164eb3
Eric Yakimoff <eyakimoff@myseneca.ca>
2025-01-25 01:18:57 -0500

    added charts

--- Home.py
+++ Home.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import streamlit as st
-from data_cleaning import get_dataframe
+from utils.data_cleaning import get_dataframe
+import plotly.express as px
 
 df = get_dataframe()
 
 st.title("Quarterback Refined Passer Rating Database")
 st.subheader("Overview")
 st.write(
-    "Welcome to the homepage. Below is a table showing the refined passer rating leaderboards. You can toggle between a table which shows extensive game Details, and a simplified table with less game data. This showcases the difference between traditional passer rating formula and the refined version."
+    "Welcome to the homepage. Below is a table showing the refined passer rating leaderboards. You can toggle between a table which shows extensive game Details, and a simplified table with less game data. This showcases the difference between the traditional passer rating formula and the refined version."
 )
 
 if "extensive" not in st.session_state:
@@ -34,3 +35,32 @@ if st.session_state.extensive:
     )
 else:
     st.dataframe(df)
+
+# dataframe with top 25 performances
+
+df_top25 = df.nlargest(25, "Refined Passer Rating")
+
+fig = px.scatter(
+    df_top25,
+    x=df_top25["Year"],
+    y=df_top25["Refined Passer Rating"],
+    text=df_top25["Name"],
+    hover_data={
+        "Refined Passer Rating": True,
+        "Year": True,
+        "Week": True,
+    },
+    title="Refined Passer Rating Top 25 Performances",
+)
+
+# Customize the layout
+fig.update_traces(
+    marker=dict(size=6, color="royalblue", line=dict(width=2, color="black"))
+)
+fig.update_layout(
+    hoverlabel=dict(bgcolor="blue", font_size=12, font_family="sans serif"),
+    xaxis_title="Year",
+    yaxis_title="Refined Passer Rating",
+    title_font_size=20,
+)
+st.plotly_chart(fig)
---
+++ pages/1_Individual_Player_Analysis.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from utils.data_cleaning import get_dataframe
+from streamlit_tags import st_tags, st_tags_sidebar
+
+df = get_dataframe()
+
+st.title("Individual Player Data")
+
+try:
+    selected_player = st_tags(
+        label="Player Name",
+        text="enter one player name",
+        suggestions=df["Name"].tolist(),
+        maxtags=1,
+    )
+    if selected_player and selected_player[0].lower() not in [
+        name.lower() for name in df["Name"].tolist()
+    ]:
+        raise KeyError("Player not found in the list.")
+except KeyError as e:
+    st.error(f"Error: {e}")
+print(str(selected_player))
